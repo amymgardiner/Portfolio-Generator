288562657219e26f80a51400390221b6aafb8a37
Amy Gardiner <amymgardiner@gmail.com>
2022-07-02 18:49:00 -0500

    added functions using inquirer to get answers from users and store information in an array

--- app.js
+++ app.js
@@ -21,12 +21,17 @@ const promptUser = () => {
   ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
   console.log(`
   =================
   Add a New Project
   =================
   `);
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
   return inquirer.prompt([
     {
       type: 'input',
@@ -61,13 +66,22 @@ const promptProject = () => {
       message: 'Would you like to enter another project?',
       default: false
     }
-  ]);
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
 };
 
 promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
 
 
 
